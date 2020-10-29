0a64fce37c4c0a00afe647e65ecf9dececb62bba
Cxarli <10348289+Cxarli@users.noreply.github.com>
2020-10-30 00:55:30 +0100

    new command format

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const TOKEN = require('fs').readFileSync('./TOKEN').toString().trim();
 
 const gt = require('translation-google');
 const tg = require('node-telegram-bot-api');
-const LANGS = require('google-translate-api').languages;
+const LANGS = {
+	getCode: function(desiredLang) {
+    	if (!desiredLang) return false;
+    	const dl = desiredLang.toLowerCase();
+    	if (dl in gt.languages) return dl;
+    	return false;
+	},
+};
 
 const bot = new tg(TOKEN, { polling: true });
 
 
 
-let num_this_hour = +process.argv[2];
+let num_this_hour = +process.argv[2] || 0;
 let this_hour = (new Date).getHours();
 const LIMIT = 50;
 
@@ -58,6 +65,10 @@ function user(obj) {
 	return str.trim();
 }
 
+function escape(x) { return x.replace(/&/g, '&amp;').replace(/>/g, '&gt;').replace(/</g, '&lt;'); }
+
+function nonl(x) { return x.replace(/\\/g,'\\\\').replace(/\n/g, '\\n').replace(/\r/g, '\\r'); }
+
 (async function() {
 	'use strict';
 
@@ -66,39 +77,93 @@ function user(obj) {
 		const me = await bot.getMe();
 		// console.debug({ me });
 		console.debug(`Connected as ${user(me)}`);
+		const MY_ID = me.id;
 
-		bot.onText(/^\/tr (from:[a-z-]+ )?([a-z-]+)( .+)?/i, async (msg, match) => {
+		bot.onText(/^\/t[rl]/i, async (msg) => {
 			// console.debug({ msg, match });
-			console.debug(`${(new Date).toISOString()} ## ${num_this_hour+1} @@ ${user(msg.from)} :: ${msg.text}`)
 
 			try {
 				const rmsg = msg.reply_to_message;
 				const emsg = rmsg || msg;
-				let froml = LANGS.getCode(match[1] ? match[1].split('from:')[1].trim() : 'auto');
-				const tol = LANGS.getCode(match[2]);
-				const fromt = (match[3] || (rmsg ? (rmsg.text || rmsg.caption) : '') || '').trim();
-				// console.debug({ froml, tol, fromt });
-				console.debug(`${froml} -> ${tol} :: ${fromt}`);
+			
+				console.debug(`${(new Date).toISOString()} ## ${num_this_hour+1} @@ ${user(msg.from)} :: ${nonl(msg.text)} %% ${rmsg ? nonl(rmsg.caption || rmsg.text) : '--'}`)
 
-				if (!fromt) return void reply(msg, "No text to translate.");
-				if (!froml) return void reply(msg, "Can't understand 'from' language.");
-				if (!tol) return void reply(msg, "Can't understand 'to' language.");
+				/*
+/tr
+/tr TLANG
+/tr SLANG TLANG
 
-				if (!quota()) return void reply(msg, "Sorry, too many translations. Please try again next hour.");
+all optionally followed by newline(s) and a message
+				 */
+				let lines = msg.text.split('\n');
+				const parts = lines.shift().substring("/tr".length).trim().split(' ').filter(x => x);
+				lines = lines.join('\n').trim();
 
-				const tres = await gt(fromt, { from: froml, to: tol });
-				const ffroml = tres.from.language.iso;
-				if (ffroml !== froml) froml = `<i>${ffroml}</i>`;
-				const tr = tres.text;
-				if (!tr) return void reply(msg, "Translation failed");
+				// console.debug({ lines, parts });
 
-				reply(emsg, `<b>${froml} -> ${tol}</b>\n\n${tr}`);
+				let slang = '', tlang = '', input = '';
+
+				if (parts.length === 1) tlang = parts.shift();
+				else if (parts.length === 2) { slang = parts.shift(); tlang = parts.shift(); }
+
+				switch (true) {
+					case !!lines:
+						input = lines;
+						break;
+
+					case !rmsg: break;
+
+					case rmsg.from.id === MY_ID:
+						const a = rmsg.text.split('\n');
+						const trl = a.shift().trim();
+						input = a.join('\n').trim();
+
+						if (slang === '' && /^[a-z-]+ -> [a-z-]+$/i.test(trl)) {
+							slang = trl.split('->').pop().trim();
+							break;
+						}
+
+						else { /* FALLTHROUGH */ }
+					default:
+						input = rmsg.caption || rmsg.text || '';
+				}
+
+
+				if (slang.toLowerCase() === 'zh') slang = 'zh-cn';
+				if (tlang.toLowerCase() === 'zh') tlang = 'zh-cn';
+
+				// console.debug({ slang, tlang, input });
+
+				if (!slang) slang = 'auto';
+				if (!tlang) tlang = 'en';
+
+				slang = LANGS.getCode(slang);
+				tlang = LANGS.getCode(tlang);
+
+				if (!slang) return void reply(msg, "&gt;w&lt; sowwy but i cant trawswate from that wangwage");
+				if (!tlang) return void reply(msg, "&gt;w&lt; sowwy but i cant trawswate to that wangwage");
+
+				if (!input) return void reply(msg, "Huuuhhhhhhhh where is the text O-O... i dont see any text to twanswate");
+
+				console.debug(`${slang} -> ${tlang} :: ${nonl(input)}`);
+
+				if (!quota()) return void reply(msg, "WWAAAAAaaahhhh wewe hit the wimit &gt;~&lt; mommy google wont let me twawswate more ;-;");
+
+				const tres = await gt(input, { from: slang, to: tlang });
+
+				const r_slang = tres.from.language.iso;
+				if (r_slang !== slang) slang = `<i>${r_slang}</i>`;
+
+				const output = tres.text;
+				if (!output) return void reply(msg, "mommy google is mad... she didnt twanswate for me... ;-;");
+
+				reply(emsg, `<b>${slang} -> ${tlang}</b>\n\n${escape(output)}`);
 
 			} catch (x) {
-				console.error({ msg, match });
+				console.error({ date: new Date, msg });
 				console.error(x);
 
-				return void reply(msg, "Sorry, I crashed. Please leave me alone for a while so I can recover.");
+				return void reply(msg, "[x_x] i dieded. maybe ask cxarli for help?");
 			}
 		});
 
